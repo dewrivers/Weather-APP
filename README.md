03afabe29022f91ed98dd00feb5d7a5c9b5170b3
Rocio Rios <rrocio46@yahoo.es>
2020-08-10 18:09:54 -0400

    finish

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 #Weather Dashboard
 
-What can this APP do?
-How can this APP help YOU?
+##What can this APP do?
+You can search for the city where you want to travel or where you live and know what the weather temperature, humidity, wind speed and UVI indicator are.
+The UVI indicator tells you in purple that it is severe, in yellow it is moderate, and in green it is low.
+It also tells you the next five days how the temperature and humidity can be.
+##How can this APP help YOU?
+This app can help you prevent high weather temperatures, rainy days, and know when you have to use sunscreen, it is important to be protected from high UVI indexes
 How this APP was build?
+
+##This app was built with 
+*JQuery
+*IPA
+*Bootstrap
+*css
+
+![](assets/images/demo.mov)
+
+
--- js/script.js
+++ js/script.js
@@ -1,31 +1,38 @@
 // MOMENT FOR THE CURRENT DAY AND TIME
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-
+var cityList = JSON.parse(localStorage.getItem("cityList")) || []
 
 //EVENT LISTENER FOR SEARCH BUTTON 
 $(".search").on("click", function (event) {
     event.preventDefault();
+    var city = $(".search-control").val().trim()
+    getForcast(city)
+})
+
+function getForcast(cityName) {
     var apikey = "dc65ec67ee0c11e97e2c814b38aa1f9f"
     //SELECT CITY FROM INPUT
-    var cityName = $(".search-control").val().trim()
     //console.log(cityName)
-    var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}`
+    var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apikey}`
 
 
     console.log("Searching City: " + cityName)
-    //make api call using ajax with city name
+    //MAKE API CALL USING AJAX WITH CITY NAME
     $.get(queryUrl, function (res) {
         var lat = res.coord.lat
         var long = res.coord.lon
-        var oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
-        units=Imperial&appid=${apikey}`
+        var oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=${apikey}`
 
-
-
-        let searchElement = $(`<li class="list-group-item city-list-item">${cityName}</li>`);
-        $('.city-list').append(searchElement);
-
+        var found = cityList.find(function (el) {
+            return el === cityName
+        })
+        if (!found) {
+            cityList.unshift(cityName)
+            localStorage.setItem('cityList', JSON.stringify(cityList))
+            let searchElement = $(`<li class="list-group-item city-list-item">${cityName}</li>`);
+            $('.city-list').append(searchElement);
+        }
 
         $.get(oneCallUrl, function (data) {
             console.log(data);
@@ -33,17 +40,32 @@ $(".search").on("click", function (event) {
             data.daily.map((card, index) => {
                 // console.log("card: ", card);
                 if (index < 6) {
-                    let header = `Day # ${index + 1}`;
+
+                    let header = moment.unix(card.dt).format("MMMM Do YYYY")
                     let humidity = `Humidity: ${card.humidity}`;
                     let temp = `Temperature: ${card.temp.day}`;
                     let windSpeed = `Wind Speed: ${card.wind_speed}`;
                     let uvIndex = `UV Index: ${card.uvi}`;
-
                     let iconURL = `https://openweathermap.org/img/wn/${card.weather[0].icon}.png`;
                     let iconElement = $(`<img src='${iconURL}' alt="Weather Icon" height="50" width="50" />`);
 
-                    //UPDATE HTML WITH CURRENT WEATHER DATA 
+                    if (card.uvi > 5) {
+                        $(".uvIndex-0").addClass("severe");
+                        $(".severe").css('color', "purple");
+                    };
 
+                    if (card.uvi <= 2) {
+                        $(".uvIndex-0").addClass("good");
+                        $(".good").css("color", "green");
+
+                    };
+                    if (card.uvi <= 8) {
+                        $(".uvIndex-0").addClass("moderate");
+                        $(".moderate").css("color", "yellow");
+
+                    };
+
+                    //UPDATE HTML WITH CURRENT WEATHER DATA 
                     $(`.card-title-${index}`).empty();
                     $(`.city`).html("<h1>" + res.name + "</h1>");
                     $('.city').append(iconElement);
@@ -54,25 +76,29 @@ $(".search").on("click", function (event) {
                     $(`.uvIndex-${index}`).text(uvIndex);
                     $(`.temp-${index}`).text(temp + ` ºF`);
                 }
-
             });
-
-
-        })
-        searchElement.on(`click`, function (event) {
-
-            console.log(event.target.html, cityName);
-            //get the city out of the event.target
-            //Set the .search-contorl element to be the city
-            $(`.search-control`).val().trim()
-                //send a click to the .search element
-                .searchElement.on(`click`)
-            //console.log(`clicked li`)
         })
         // log out response
         console.log(res)
 
-
-
     })
+}
+
+function renderCityButtons() {
+    for (let i = 0; i < cityList.length; i++) {
+        let searchElement = $(`<li class="list-group-item city-list-item">${cityList[i]}</li>`);
+        $('.city-list').append(searchElement)
+    }
+    if (cityList.length > 0) {
+        getForcast(cityList[0])
+    }
+}
+
+renderCityButtons();
+
+$(document).on(`click`, '.city-list-item', function (event) {
+    //console.log(event.target)
+    var city = $(this).text()
+    //console.log(city)
+    getForcast(city)
 })
